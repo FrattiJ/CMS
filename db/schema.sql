c75005e810d54cbcd7652a98ec9c1ba91e10a930
Jacob Fratti <JacobRFratti@gmail.com>
2023-11-16 20:39:57 -0500

    added primary and foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -3,22 +3,34 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department (
-    id INT NOT NULL, --Needs Primary key
-    name VARCHAR(30) NOT NULL
+CREATE TABLE departments (
+    id INT NOT NULL,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-    id INT NOT NULL, --Needs Primary key
+CREATE TABLE roles (
+    id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL --needs to hold reference to department role belongs to
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
 );
 
-CREATE TABLE employee (
-    id INT NOT NULL, -- needs Primary key
+CREATE TABLE employees (
+    id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL, -- needs to hold reference to employee role
-    manager_id INT -- Needs to hold reference to another employee that is the manager of the current employee
+    role_id INT NOT NULL,
+    manager_id INT
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
+    ON DELETE SET NULL
 );
